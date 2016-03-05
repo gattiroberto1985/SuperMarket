666e19035c9a37ca9a8ad82c0cabe978e21ab83e
debianbob <a@b.c>
2016-03-05 23:42:32 +0100

    Inserimento licenza. Sistemazione bug in avvio applicazione:  - actionbar a null in activity  - findviewbyid in fragmentlist chiamato su activity e non su view  - cambio id per la lista di spese

--- src/org/bob/android/supermarket/gui/fragments/FragmentExpenseList.java
+++ src/org/bob/android/supermarket/gui/fragments/FragmentExpenseList.java
@@ -1,3 +1,27 @@
+/*
+ *  The MIT License (MIT)
+ *
+ * Copyright (c) $date.year $user.name
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 package org.bob.android.supermarket.gui.fragments;
 
 import android.app.Activity;
@@ -77,7 +101,7 @@ public class FragmentExpenseList extends ListFragment
     {
         Logger.lfc_log( "ListFragment: onCreateView");
         View v = inflater.inflate(R.layout.fragment_expense_list, container);
-        this.expenseLV = (ListView) this.getActivity().findViewById(R.id.frg_exp_list_listview);
+        this.expenseLV = (ListView) v.findViewById(android.R.id.list);
         this.expenseLV.setAdapter(new AdapterExpenses(this.getActivity()));
         return v;
     }
